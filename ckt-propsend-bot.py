3353c254f9e0baba0f96c31baad06bb2c7b668fb
yzcdgy <ypycadigoy@gmail.com>
2019-08-05 02:01:32 +0800

    Updated company/email lists from Text to ScrolledText

--- ckt-propsend-bot.py
+++ ckt-propsend-bot.py
@@ -1,6 +1,7 @@
 import io
 import os
 
+import tkinter.scrolledtext as tkst
 from tkinter import *
 from tkinter import filedialog
 from tkinter.colorchooser import askcolor
@@ -71,16 +72,26 @@ class text_font_setter(Frame):
 
         self.label_for_input.grid(row=0, column=0, columnspan=3, padx=10, pady=4)
         self.text_font_dropdown.grid(row=0, column=3, columnspan=3, padx=(52, 0), pady=4)
+class input_date_setter(Frame):
+    def __init__(self, parent, *args, **kwargs):
+        Frame.__init__(self, parent, *args, **kwargs)
+        self.parent = parent
+
+        self.label_for_input = Label(self, text="Set date input")
+        self.date_input = Entry(self, width = 19, font = ("Helvetica", 11))
+        self.date_input.insert(END, '21 Nov 2015')
+
+        self.label_for_input.grid(row=0, column=0, columnspan=3, padx=10, pady=4)
+        self.date_input.grid(row=0, column=3, columnspan=3, padx=(46, 0), pady=4)
 class text_size_setter(Frame):
     def __init__(self, parent, *args, **kwargs):
         Frame.__init__(self, parent, *args, **kwargs)
-        #self.text_size = None
         self.parent = parent
         self.var = IntVar(self)
         self.var.set(12)
 
         self.label_for_size = Label(self, text="Set overlay text size")
-        self.size_entry_box = Spinbox(self, font = 14, width = 16, from_=0, to=100, state='readonly', textvariable=self.var)
+        self.size_entry_box = Spinbox(self, font = ("Helvetica",11), width = 18, from_=0, to=100, state='readonly', textvariable=self.var)
 
         self.label_for_size.grid(row = 0, column = 0, columnspan = 3, padx = 10, pady = (6,0))
         self.size_entry_box.grid(row = 0, column = 3, columnspan = 3, padx = (17,0), pady = (6,0))
@@ -96,12 +107,12 @@ class text_loc_setter(Frame):
         self.label_for_loc = Label(self, text="Set overlay text position")
         self.label_for_x = Label(self, text="x-position")
         self.label_for_y = Label(self, text="y-position")
-        self.loc_entry_x = Spinbox(self, font = 14, width = 6, from_=0, to=1000, state='readonly', textvariable = self.varx)
-        self.loc_entry_y = Spinbox(self, font = 14, width = 6, from_=0, to=1000, state='readonly', textvariable = self.vary)
+        self.loc_entry_x = Spinbox(self, font = ("Helvetica",11), width = 7, from_=0, to=1000, state='readonly', textvariable = self.varx)
+        self.loc_entry_y = Spinbox(self, font = ("Helvetica",11), width = 7, from_=0, to=1000, state='readonly', textvariable = self.vary)
 
         self.label_for_x.grid(row = 0, column = 3, columnspan = 3)
         self.label_for_y.grid(row = 0, column=6, columnspan=3)
-        self.label_for_loc.grid(row = 1, column = 0, columnspan = 3, padx = (10, 2))
+        self.label_for_loc.grid(row = 0, rowspan = 2, column = 0, columnspan = 3, padx = (10, 2))
         self.loc_entry_x.grid(row = 1, column = 3, columnspan = 3, padx = (5,5), pady = (0, 15))
         self.loc_entry_y.grid(row=1, column=6, columnspan=3, padx=(5,5), pady = (0,15))
 class pdf_file_opener(Frame):
@@ -123,29 +134,29 @@ class companies_list(Frame):
         Frame.__init__(self, parent, *args, **kwargs)
         self.parent = parent
 
-        self.companies_frame = Frame(self.parent, width = 300, height = 250)
+        #self.companies_frame = Frame(self.parent, width = 300, height = 278)
 
-        self.label = Label(self.companies_frame, text="List of companies", pady = 3)
-        self.companies_text = Text(self.companies_frame, height = 16)
+        self.label = Label(self, text="List of companies", pady = 3)
+        self.companies_text = tkst.ScrolledText(self, height = 16, width = 35)
+
         self.label.grid(sticky = W)
         self.companies_text.grid()
-
-        self.companies_frame.grid(row = 1, column = 1, rowspan = 10, padx = 5, sticky = W)
-        self.companies_frame.grid_propagate(False)
+        #self.companies_frame.grid(row = 1, column = 1, rowspan = 10, padx = 5, sticky = W)
+        #self.companies_frame.grid_propagate(False)
 class emails_list(Frame):
     def __init__(self, parent, *args, **kwargs):
         Frame.__init__(self, parent, *args, **kwargs)
         self.parent = parent
 
-        self.emails_frame = Frame(self.parent, width = 300, height = 250)
+        #self.emails_frame = Frame(self.parent, width = 300, height = 278)
 
-        self.label = Label(self.emails_frame, text="List of emails", pady = 3)
-        self.emails_text = Text(self.emails_frame, height = 16)
+        self.label = Label(self, text="List of emails", pady = 3)
+        self.emails_text = tkst.ScrolledText(self, height = 16, width = 35)
+
         self.label.grid(sticky = W)
         self.emails_text.grid()
-
-        self.emails_frame.grid(row = 1, column = 2, rowspan = 10, padx = 5, sticky = W)
-        self.emails_frame.grid_propagate(False)
+        #self.emails_frame.grid(row = 1, column = 2, rowspan = 10, padx = 5, sticky = W)
+        #self.emails_frame.grid_propagate(0)
 
 class pdf_processor(Frame):
     def __init__(self, parent, *args, **kwargs):
@@ -164,16 +175,18 @@ class pdf_processor(Frame):
         self.pdf_file_opener = pdf_file_opener(self)
         self.companies_list = companies_list(self)
         self.emails_list = emails_list(self)
+        self.input_date_setter = input_date_setter(self)
 
         self.doc_size_setter.grid(row = 1, sticky = W)
         self.text_color_setter.grid(row = 2, sticky = W)
         self.text_font_setter.grid(row = 3, sticky = W)
-        self.text_size_setter.grid(row = 4, sticky = W)
-        self.text_loc_setter.grid(row = 5, sticky = W)
-        self.pdf_file_opener.grid(row = 6, sticky = W)
-        self.test_btn.grid(row = 7, padx = 9, pady=10)
-        self.companies_list.grid(row = 1, column = 1)
-        self.emails_list.grid(row = 1, column = 0)
+        self.input_date_setter.grid(row = 4, sticky = W)
+        self.text_size_setter.grid(row = 5, sticky = W)
+        self.text_loc_setter.grid(row = 6, sticky = W)
+        self.pdf_file_opener.grid(row = 7, sticky = W)
+        self.test_btn.grid(row = 8, padx = 9, pady = 5)
+        self.companies_list.grid(row = 1, column = 1, rowspan = 8, pady = 5)
+        self.emails_list.grid(row = 1, column = 2, rowspan = 8, pady = 5)
 
     def testOutput(self):
         self.pdf_packet = io.BytesIO()
@@ -182,9 +195,9 @@ class pdf_processor(Frame):
         self.can.setFillColor(HexColor(self.text_color_setter.text_color))
         self.can.setFont(self.text_font_setter.text_font, int(self.text_size_setter.size_entry_box.get()))
         self.can.drawString(int(self.text_loc_setter.loc_entry_x.get()), int(self.text_loc_setter.loc_entry_y.get()),
-                            '7 May 2019 TEST DATE')
+                            self.input_date_setter.date_input.get())
         self.can.drawString(int(self.text_loc_setter.loc_entry_x.get()),
-                            int(self.text_loc_setter.loc_entry_y.get()) - 8, 'Tesla Motors Inc. TEST COMPANY')
+                            int(self.text_loc_setter.loc_entry_y.get()) - 8, 'Test Company Inc.')
         self.can.save()
 
         self.pdf_packet.seek(0)
@@ -209,12 +222,11 @@ class main_app(Frame):
 
         self.pdf_processor.grid(column = 0, row = 0)
 
-
 if __name__ == "__main__":
 
     root = Tk()
     root.title("PropSend Bot - ypycadigoy upckt18A")
-    root.geometry("940x500")
+    root.geometry("925x500")
     main = main_app(root)
     main.pack(side="top", fill="both", expand=True)
     root.mainloop()
